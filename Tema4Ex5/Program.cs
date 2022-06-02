f46eac3924cac30faade80cd7f7f6104d5ae57cf
Gergoa123 <gergo3124@gmail.com>
2022-06-02 11:51:03 +0300

    Add project files.

---
+++ Tema4Ex5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+//Ex5 Scrieti o functie recursiva care va calcula suma numerelor de la 1 pana la n,apelati - o si afisati-i rezultatul. 
+
+namespace Tema4Ex5
+{
+    class Program
+    {
+        static int Sum(int n)
+        {
+            
+            if (0 == n )
+            {
+                return 0;
+            }
+            return n + Sum(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti n = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(Sum(a));
+        }
+
+        
+    }
+}
